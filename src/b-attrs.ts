0183930abe9678d57be4274360ba1bcd79aacb8e
Nobuhito Ibaraki <nobuhito.ibaraki@plaid.co.jp>
2023-07-13 19:33:25 +0900

    Default data

--- src/b-attrs.ts
+++ src/b-attrs.ts
@@ -1,52 +1,4 @@
-let data = {
-  table1: [
-    { name: 'taro', sex: 'male', age: 28 },
-    { name: 'hanako', sex: 'female', age: 32 },
-    { name: 'mike', sex: 'male', age: 21 },
-    { name: 'jane', sex: 'female', age: 25 },
-    { name: 'james', sex: 'male', age: 33 },
-  ],
-  table2: [
-    { name: 'yamada', sex: 'male', job: 'engineer', roles: ['manager', 'engineer'] },
-    { name: 'suzuki', sex: 'female', job: 'designer', roles: ['head', 'designer'] },
-    { name: 'sato', sex: 'male', job: 'sales', roles: ['sub'] },
-    { name: 'tanaka', sex: 'female', job: 'researcher', roles: [] },
-  ],
-  table3: [
-    { name: 'a', imageUrl: 'https://cdn.goope.jp/45227/161123133219uvsp_l.jpg' },
-    { name: 'b', imageUrl: 'https://cdn.goope.jp/45227/161123133242xhrd_l.jpg' },
-    { name: 'c', imageUrl: 'https://cdn.goope.jp/45227/161123133257tc3t_l.jpg' },
-    { name: 'd', imageUrl: 'https://cdn.goope.jp/45227/161123133322kjgc_l.jpg' },
-    { name: 'e', imageUrl: 'https://cdn.goope.jp/45227/161123133340ta2s_l.jpg' },
-  ],
-  tableGroup1: {
-    fruits: [
-      {
-        name: 'orange',
-      },
-      {
-        name: 'apple',
-      },
-      {
-        name: 'grape',
-      },
-    ],
-    animals: [
-      {
-        name: 'cat',
-      },
-      {
-        name: 'dog',
-      },
-      {
-        name: 'bear',
-      },
-      {
-        name: 'rabbit',
-      },
-    ]
-  },
-}
+let data = {}
 
 function extractDataByJSExpr(data: any, exprs: string[]): { $: any; $root: any } {
   const root = data;
@@ -73,7 +25,7 @@ const ATTR_IF = 'data-b-if';
 const ATTR_IF__HIDDEN = 'data-b-if_hidden';
 
 const EVENT_RENDERED = 'b:rendered'
-const EVENT_SET_DATA = 'b:setData'
+const EVENT_SET_DATA = 'b:set-data'
 
 function is(element: Element, attr: string) {
   return element.getAttribute(attr) !== null;
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,59 @@
+const sampleData = {
+  table1: [
+    { name: 'taro', sex: 'male', age: 28 },
+    { name: 'hanako', sex: 'female', age: 32 },
+    { name: 'mike', sex: 'male', age: 21 },
+    { name: 'jane', sex: 'female', age: 25 },
+    { name: 'james', sex: 'male', age: 33 },
+  ],
+  table2: [
+    { name: 'yamada', sex: 'male', job: 'engineer', roles: ['manager', 'engineer'] },
+    { name: 'suzuki', sex: 'female', job: 'designer', roles: ['head', 'designer'] },
+    { name: 'sato', sex: 'male', job: 'sales', roles: ['sub'] },
+    { name: 'tanaka', sex: 'female', job: 'researcher', roles: [] },
+  ],
+  table3: [
+    { name: 'a', imageUrl: 'https://cdn.goope.jp/45227/161123133219uvsp_l.jpg' },
+    { name: 'b', imageUrl: 'https://cdn.goope.jp/45227/161123133242xhrd_l.jpg' },
+    { name: 'c', imageUrl: 'https://cdn.goope.jp/45227/161123133257tc3t_l.jpg' },
+    { name: 'd', imageUrl: 'https://cdn.goope.jp/45227/161123133322kjgc_l.jpg' },
+    { name: 'e', imageUrl: 'https://cdn.goope.jp/45227/161123133340ta2s_l.jpg' },
+  ],
+  tableGroup1: {
+    fruits: [
+      {
+        name: 'orange',
+      },
+      {
+        name: 'apple',
+      },
+      {
+        name: 'grape',
+      },
+    ],
+    animals: [
+      {
+        name: 'cat',
+      },
+      {
+        name: 'dog',
+      },
+      {
+        name: 'bear',
+      },
+      {
+        name: 'rabbit',
+      },
+    ]
+  },
+}
+
+window.dispatchEvent(new CustomEvent('b:set-data', {
+  detail: {
+    data: sampleData,
+  },
+}));
+
 
 // dynamic content
 document.getElementById('app')!.innerHTML = `
